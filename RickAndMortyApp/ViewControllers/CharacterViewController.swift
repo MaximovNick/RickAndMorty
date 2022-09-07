3664fcea5ecb1cda21d6b37708e8da7e6cd3297a
Nikolai Maximov <kolkamaximov@gmail.com>
2022-09-08 01:17:40 +0300

    Character View Controller: add Table View setup Navigation Bar Appearance

--- RickAndMortyApp/ViewControllers/CharacterViewController.swift
+++ RickAndMortyApp/ViewControllers/CharacterViewController.swift
@@ -9,14 +9,87 @@ import UIKit
 
 class CharacterViewController: UIViewController {
     
+    // MARK - Private Properties
+    private var rickAndMorty: RickAndMorty?
     
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(CharacterTableViewCell.self, forCellReuseIdentifier: CharacterTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 70
+        title = "Character"
         
-        view.backgroundColor = .blue
-     
+        setSubviews()
+        setDelegates()
+        setConstraints()
+        setupNavigationBar()
     }
     
+    private func setSubviews() {
+        view.addSubview(tableView)
+    }
     
+    private func setDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupNavigationBar() {
+        title = "Rick & Morty"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = .black
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    
+    private func fetchData(from url: String?) {
+        
+    }
+
+}
+
+
+// MARK - UITableViewDelegate, UITableViewDataSource
+extension CharacterViewController: UITableViewDelegate, UITableViewDataSource {
+    
+   
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CharacterTableViewCell.identifier, for: indexPath) as? CharacterTableViewCell else { return UITableViewCell()}
+        
+        
+        
+        return cell
+    }
+    
+}
+
+
+extension CharacterViewController {
+    
+    func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        ])
+    }
 }
