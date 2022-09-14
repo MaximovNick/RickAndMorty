9da2029167ae89f1d06938b5b3a24214fd29108b
Nikolai Maximov <kolkamaximov@gmail.com>
2022-09-14 23:39:10 +0300

    Изменение метода FetchImage

--- RickAndMortyApp/Services/NewtworManager.swift
+++ RickAndMortyApp/Services/NewtworManager.swift
@@ -88,14 +88,15 @@ class NetworkManager {
     }
     
     // MARK: - Fetch Image
-    func fetchImage(from url: String?, with completion: @escaping(Data) -> Void) {
-        guard let stringURL = url else { return }
-        guard let imageURL = URL(string: stringURL) else { return}
-        DispatchQueue.global().async {
-            guard let data = try? Data(contentsOf: imageURL) else { return }
-            DispatchQueue.main.async {
-                completion(data)
+    func fetchImage(from url: URL, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
             }
-        }
+            DispatchQueue.main.async {
+                completion(.success(data))
+            }
+        }.resume()
     }
 }
